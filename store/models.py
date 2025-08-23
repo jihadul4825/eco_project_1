205a7d7b2732078196959ee8d12ee3e84ad0bb3c
jihadul4825 <jihadul4825@gmail.com>
2025-08-24 03:05:40 +0600

    isssue solve for search functionality

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from category.models import Category
 from django.urls import reverse
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 class Product(models.Model):
--- store/views.py
+++ store/views.py
@@ -1,19 +1,27 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product 
 from category.models import Category
 from cart.models import CartItem
-from django.db.models import Q
 
 from cart.views import _cart_id
+
+# from .forms import ReviewForm
+from orders.models import OrderProduct
+
+from django.contrib import messages
+from django.db.models import Q
 from django.core.paginator import Paginator
 
+
+
 def store(request, category_slug=None):
-    categories = None
+    # categories = None
     products = None
 
     if category_slug != None:
-        category = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category=category, is_available=True)
+        # category = get_object_or_404(Category, slug=category_slug)
+        # products = Product.objects.filter(category=category, is_available=True)
+        products = Product.objects.select_related('category').filter(category__slug=category_slug, is_available=True)
         
         paginator = Paginator(products, 1)
         page = request.GET.get('page')
@@ -21,7 +29,8 @@ def store(request, category_slug=None):
         
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True).order_by('id')
+        # products = Product.objects.all().filter(is_available=True).order_by('id')
+        products = Product.objects.select_related().filter(is_available=True).order_by('id')
         
         paginator = Paginator(products, 3)
         page = request.GET.get('page')
@@ -38,8 +47,10 @@ def store(request, category_slug=None):
 
 def product_detail(request, category_slug, product_slug):
     try:
-        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
-        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+        # single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        single_product = Product.objects.select_related('category').get(category__slug=category_slug, slug=product_slug)
+        # in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+        in_cart = CartItem.objects.prefetch_related('product', 'cart').filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
     
@@ -54,12 +65,26 @@ def search(request):
     if 'keyword' in request.GET:
         keyword = request.GET['keyword']
         if keyword:
-            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
-            
+            products = Product.objects.select_related('category').filter(
+                Q(description__icontains=keyword) | Q(product_name__icontains=keyword),
+                is_available=True
+            ).order_by('id')
             product_count = products.count()
 
-    context = {
-        'products': products,
-        'product_count': product_count,
-    }
-    return render(request, 'store/store.html', context)
+            # Pagination
+            paginator = Paginator(products, 3)
+            page = request.GET.get('page')
+            paged_products = paginator.get_page(page)
+            
+            print(request.GET)
+
+            context = {
+                'products': paged_products,
+                'product_count': product_count,
+            }
+            return render(request, 'store/store.html', context)
+        else:
+            # If keyword is empty, redirect to store
+            return redirect('store')
+    else:
+        return redirect('store')
